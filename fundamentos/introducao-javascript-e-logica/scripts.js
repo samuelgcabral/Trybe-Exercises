7f167f5d91a314ab0a37c4732d9a6cf6c3e65892
samuelgcabral <samuel.gc1406@gmail.com>
2022-05-12 13:32:24 -0300

    modificações no readme e folder pro javascript

---
+++ fundamentos/introducao-javascript-e-logica/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Samuel"
+const birthCity = "Blumenau"
+var birthYear = 2001
+birthYear = 2030
+birthCity = "Tokyo"
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
