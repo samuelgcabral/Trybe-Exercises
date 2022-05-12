507c2ce88739bbb25436fe66454d499edbab58ee
samuelgcabral <samuel.gc1406@gmail.com>
2022-05-12 19:08:38 -0300

    .

--- fundamentos/introducao-javascript-e-logica/scripts.js
+++ fundamentos/introducao-javascript-e-logica/scripts.js
@@ -1,8 +1,22 @@
-const myName = "Samuel"
-const birthCity = "Blumenau"
-var birthYear = 2001
-birthYear = 2030
-birthCity = "Tokyo"
-console.log(myName)
-console.log(birthCity)
-console.log(birthYear)
+let weekDay = "terça-feira"
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
+else if (weekDay == "sábado" || weekDay == "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU")
+}
+
+let situacaoCandidato = "lista"
+switch(situacaoCandidato) {
+    case "aprovado":
+        console.log("Parabéns, você foi aprovada(o)!");
+        break;
+    case "lista":
+        console.log("Você está na nossa lista de espera");
+        break;
+    case "reprovado":
+        console.log("Você foi reprovada(o)");
+        break;
+    default:
+        console.log("não se aplica");
+}
