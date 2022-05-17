eb4d648c7d5156d10e8efbe51433eabefd271612
samuelgcabral <samuel.gc1406@gmail.com>
2022-05-17 20:51:00 -0300

    exercícios e readme

--- fundamentos/introducao-javascript-e-logica/scripts.js
+++ fundamentos/introducao-javascript-e-logica/scripts.js
@@ -1,149 +1,273 @@
-const a = 15
-const b = 15
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
-///////////////////////////////////////////////
-const x = 31
-const y = 30
-const z = 33
-let maior = x
-    if (y > x) {
-        maior = y
-    }
-    if (z > y) {
-        maior = z
-    }
-    console.log(maior);
-////////////////////////////////////////////////
-const numero = 0
-if (numero > 0) {
-    console.log("positivo");
-}else if (numero < 0) {
-    console.log("negativo");
-}else {
-    console.log("zero");
-}
-///////////////////////////////////////////////////////
-const angulo1 = 61
-const angulo2 = 60
-const angulo3 = 60
-if (angulo1 + angulo2 + angulo3 === 180) {
-    console.log(true);
-}else {
-    console.log(false);
-}
-////////////////////////////////////////////////////
-let pecaDeXadrez = "DAMA".toLowerCase()
-    switch(pecaDeXadrez) {
-        case "Cavalo".toLowerCase():
-            console.log("L");
-            break;
-        case "bispo".toLowerCase():
-            console.log("diagonais");
-            break;
-        case "rei".toLowerCase():
-            console.log("todas as direções uma casa");
-            break;
-        case "dama".toLowerCase():
-            console.log("todas as direções infinitas casas");
-            break;
-        case "torre".toLowerCase():
-            console.log("linhas retas vertical e horizontal");
-            break;
-    }
- /////////////////////////////////////////////////////
-let notaPorcentagem = 61
-    if (notaPorcentagem >= 90 && notaPorcentagem <= 100) {
-        notaPorcentagem = "A";
-        console.log(notaPorcentagem);
+// // // // const a = 15
+// // // // const b = 15
+// // // // console.log(a + b);
+// // // // console.log(a - b);
+// // // // console.log(a * b);
+// // // // console.log(a / b);
+// // // // console.log(a % b);
+// // // // ///////////////////////////////////////////////
+// // // // const x = 31
+// // // // const y = 30
+// // // // const z = 33
+// // // // let maior = x
+// // // //     if (y > x) {
+// // // //         maior = y
+// // // //     }
+// // // //     if (z > y) {
+// // // //         maior = z
+// // // //     }
+// // // //     console.log(maior);
+// // // // ////////////////////////////////////////////////
+// // // // const numero = 0
+// // // // if (numero > 0) {
+// // // //     console.log("positivo");
+// // // // }else if (numero < 0) {
+// // // //     console.log("negativo");
+// // // // }else {
+// // // //     console.log("zero");
+// // // // }
+// // // // ///////////////////////////////////////////////////////
+// // // // const angulo1 = 61
+// // // // const angulo2 = 60
+// // // // const angulo3 = 60
+// // // // if (angulo1 + angulo2 + angulo3 === 180) {
+// // // //     console.log(true);
+// // // // }else {
+// // // //     console.log(false);
+// // // // }
+// // // // ////////////////////////////////////////////////////
+// // // // let pecaDeXadrez = "DAMA".toLowerCase()
+// // // //     switch(pecaDeXadrez) {
+// // // //         case "Cavalo".toLowerCase():
+// // // //             console.log("L");
+// // // //             break;
+// // // //         case "bispo".toLowerCase():
+// // // //             console.log("diagonais");
+// // // //             break;
+// // // //         case "rei".toLowerCase():
+// // // //             console.log("todas as direções uma casa");
+// // // //             break;
+// // // //         case "dama".toLowerCase():
+// // // //             console.log("todas as direções infinitas casas");
+// // // //             break;
+// // // //         case "torre".toLowerCase():
+// // // //             console.log("linhas retas vertical e horizontal");
+// // // //             break;
+// // // //     }
+// // // //  /////////////////////////////////////////////////////
+// // // // let notaPorcentagem = 61
+// // // //     if (notaPorcentagem >= 90 && notaPorcentagem <= 100) {
+// // // //         notaPorcentagem = "A";
+// // // //         console.log(notaPorcentagem);
 
-    }
-    else if (notaPorcentagem >= 80 && notaPorcentagem < 90) {
-        notaPorcentagem = "B";
-        console.log(notaPorcentagem);
+// // // //     }
+// // // //     else if (notaPorcentagem >= 80 && notaPorcentagem < 90) {
+// // // //         notaPorcentagem = "B";
+// // // //         console.log(notaPorcentagem);
 
-    }
-    else if (notaPorcentagem >= 70 && notaPorcentagem < 80) {
-        notaPorcentagem = "C";
-        console.log(notaPorcentagem);
+// // // //     }
+// // // //     else if (notaPorcentagem >= 70 && notaPorcentagem < 80) {
+// // // //         notaPorcentagem = "C";
+// // // //         console.log(notaPorcentagem);
 
-    }
-    else if (notaPorcentagem >= 60 && notaPorcentagem < 70) {
-        notaPorcentagem = "D";
-        console.log(notaPorcentagem);
+// // // //     }
+// // // //     else if (notaPorcentagem >= 60 && notaPorcentagem < 70) {
+// // // //         notaPorcentagem = "D";
+// // // //         console.log(notaPorcentagem);
 
-    }
-    else if (notaPorcentagem >= 50 && notaPorcentagem < 60) {
-        notaPorcentagem = "E";
-        console.log(notaPorcentagem);
+// // // //     }
+// // // //     else if (notaPorcentagem >= 50 && notaPorcentagem < 60) {
+// // // //         notaPorcentagem = "E";
+// // // //         console.log(notaPorcentagem);
 
-    }
-    else if (notaPorcentagem < 50 && notaPorcentagem > 0) {
-        notaPorcentagem = "F";
-        console.log(notaPorcentagem);
+// // // //     }
+// // // //     else if (notaPorcentagem < 50 && notaPorcentagem > 0) {
+// // // //         notaPorcentagem = "F";
+// // // //         console.log(notaPorcentagem);
 
-    } else {
-        console.log("Erro crítico: Dados inválidos");
-    }
-/////////////////////////////////////////////////////////////
-const numero1 = 1
-const numero2 = 5
-const numero3 = 3
-    if (numero1 % 2 == 0 || numero2 % 2 == 0 || numero3 % 2 == 0) {
-        console.log(true);
-    }else {
-        console.log(false);
-    }
-/////////////////////////////////////////////////////////
-const numero1 = 2
-const numero2 = 6
-const numero3 = 4
-    if (numero1 % 2 != 0 || numero2 % 2 != 0 || numero3 % 2 != 0) {
-        console.log(true);
-    }else {
-        console.log(false);
-    }
-/////////////////////////////////////////////////////////
-let quantidadeDeProdutos = 1000;
-const valorCusto = 40 * quantidadeDeProdutos;
-const valorVenda = 80 * quantidadeDeProdutos;
-let imposto = valorCusto * 0.20;
-let valorCustoTotal = valorCusto + imposto;
-let lucro = valorVenda - valorCustoTotal;
-    if (valorCusto > 0 && valorVenda > 0) {
-        console.log(lucro);
-    } else {
-        console.log("Erro: dados inválidos");
-    }
-/////////////////////////////////////////////////////
-let salarioBruto = 4000.10
-let aliquotaINSS 
-let aliquotaIR
-    if (salarioBruto < 1556.94 && salarioBruto > 0) {
-        aliquotaINSS = salarioBruto * 0.08;
-    }else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-        aliquotaINSS = salarioBruto * 0.09
-    }else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
-        aliquotaINSS = salarioBruto * 0.11
-    }else if (salarioBruto > 5189.82) {
-        aliquotaINSS = 570.88
-    }
+// // // //     } else {
+// // // //         console.log("Erro crítico: Dados inválidos");
+// // // //     }
+// // // // /////////////////////////////////////////////////////////////
+// // // // const numero1 = 1
+// // // // const numero2 = 5
+// // // // const numero3 = 3
+// // // //     if (numero1 % 2 == 0 || numero2 % 2 == 0 || numero3 % 2 == 0) {
+// // // //         console.log(true);
+// // // //     }else {
+// // // //         console.log(false);
+// // // //     }
+// // // // /////////////////////////////////////////////////////////
+// // // // const numero1 = 2
+// // // // const numero2 = 6
+// // // // const numero3 = 4
+// // // //     if (numero1 % 2 != 0 || numero2 % 2 != 0 || numero3 % 2 != 0) {
+// // // //         console.log(true);
+// // // //     }else {
+// // // //         console.log(false);
+// // // //     }
+// // // // /////////////////////////////////////////////////////////
+// // // // let quantidadeDeProdutos = 1000;
+// // // // const valorCusto = 40 * quantidadeDeProdutos;
+// // // // const valorVenda = 80 * quantidadeDeProdutos;
+// // // // let imposto = valorCusto * 0.20;
+// // // // let valorCustoTotal = valorCusto + imposto;
+// // // // let lucro = valorVenda - valorCustoTotal;
+// // // //     if (valorCusto > 0 && valorVenda > 0) {
+// // // //         console.log(lucro);
+// // // //     } else {
+// // // //         console.log("Erro: dados inválidos");
+// // // //     }
+// // // // /////////////////////////////////////////////////////
+// // // // let salarioBruto = 4000.10
+// // // // let aliquotaINSS 
+// // // // let aliquotaIR
+// // // //     if (salarioBruto < 1556.94 && salarioBruto > 0) {
+// // // //         aliquotaINSS = salarioBruto * 0.08;
+// // // //     }else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+// // // //         aliquotaINSS = salarioBruto * 0.09
+// // // //     }else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+// // // //         aliquotaINSS = salarioBruto * 0.11
+// // // //     }else if (salarioBruto > 5189.82) {
+// // // //         aliquotaINSS = 570.88
+// // // //     }
     
-    if (salarioBruto <= 1903.98) {
-        aliquotaIR = 0
-    }else if (salarioBruto >= 1903.99 && salarioBruto <= 2826.65) {
-        aliquotaIR = salarioBruto * 0.075 - 142.80;
-    }else if (salarioBruto >= 2826.66 && salarioBruto <= 3751.05) {
-        aliquotaIR = salarioBruto * 0.15 - 354.80
-    }else if (salarioBruto >= 3751.06 && salarioBruto <= 4664.68) {
-        aliquotaIR = salarioBruto * 0.225 - 636.13
-    }else {
-        aliquotaIR = salarioBruto * 0.275 - 869.36
-    }
-let salarioBase = salarioBruto - aliquotaINSS - aliquotaIR
+// // // //     if (salarioBruto <= 1903.98) {
+// // // //         aliquotaIR = 0
+// // // //     }else if (salarioBruto >= 1903.99 && salarioBruto <= 2826.65) {
+// // // //         aliquotaIR = salarioBruto * 0.075 - 142.80;
+// // // //     }else if (salarioBruto >= 2826.66 && salarioBruto <= 3751.05) {
+// // // //         aliquotaIR = salarioBruto * 0.15 - 354.80
+// // // //     }else if (salarioBruto >= 3751.06 && salarioBruto <= 4664.68) {
+// // // //         aliquotaIR = salarioBruto * 0.225 - 636.13
+// // // //     }else {
+// // // //         aliquotaIR = salarioBruto * 0.275 - 869.36
+// // // //     }
+// // // // let salarioBase = salarioBruto - aliquotaINSS - aliquotaIR
 
-console.log(salarioBase);
-/////////////////////////////////////////////////////
+// // // // console.log(salarioBase);
+// // // // /////////////////////////////////////////////////////
+// // // // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// // // // let menuServices = menu[1];
+
+// // // // console.log(menuServices);
+// // // // ////////////////////////////////////////////////////////
+// // // // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// // // // let indexOfPortfolio = menu.indexOf("Portfólio");
+
+// // // // console.log(indexOfPortfolio);
+// // // // //////////////////////////////////////////////////////
+// // // // let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// // // // menu.push("Contato")
+// // // // console.log(menu);
+// // // ///////////////////////////////
+// // // let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// // //     for(let index = 0; index < groceryList.length; index += 1) {
+// // //         console.log(groceryList[index]);
+// // //     }
+// // /////////////////////
+// // let palavra = "Jesus";
+// // for (let letra of palavra) {
+// //     console.log(letra);
+// // }
+// //////////////////////////////
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+//     for(let name of names) {
+//         console.log(name);
+//     }
+/////////////////////////////////////////////////////////////
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = numbers[0] + numbers[1] + numbers[2] + numbers[3] +  numbers[4] + numbers[6]  + numbers[8] + numbers[9] 
+// let mediaAritmetica = soma / numbers.length
+//     if (mediaAritmetica > 20) {
+//         console.log("valor maior do que 20");
+//     }else {
+//         console.log("valor menor ou igual a 20");
+//     }
+// let maiorNumero = numbers[0]
+//     for (let index = 0; index < numbers.length; index += 1) {
+       
+//         if (numbers[index] > maiorNumero) {
+//             maiorNumero = numbers[index]
+            
+//         }
+        
+//     }console.log(maiorNumero);
+// let menorNumero = numbers[0]
+//     for (let index = 0; index < numbers.length; index += 1) {
+       
+//         if (numbers[index] < menorNumero) {
+//             menorNumero = numbers[index]
+            
+//         }
+        
+//     }console.log(menorNumero);
+// let numerosImpares = 0
+// let maiorNumero = numbers[0]
+//     for (let index = 0; index < numbers.length; index += 1) {
+//         if (numbers[index] % 2 !== 0) {
+//             numerosImpares++
+            
+//         } if (numerosImpares == 0) {
+//             console.log("Nenhum valor ímpar encontrado");
+//         }
+//     }console.log(numerosImpares);
+    // for (let indice = 1; indice <= 25; indice++) {
+    //         let array = []
+    //         array.push(indice / 2);
+        
+    //     console.log(array);
+        
+    // }
+////////////// Crie um algoritmo que retorne o fatorial de 10:
+    // let fator = 10
+    //     for (let index = 9; index > 0; index -= 1) {
+    //         let fatorial = fator * index
+    //         console.log(fatorial);
+            
+    //     }
+////////////////// Desenvolva um algoritmo que é capaz de inverter uma palavra
+// let word = "Luis Inácio Lula da Silva"
+// let wordinvertida = ""
+
+//     for (let index = word.length - 1; index >= 0; index -= 1) {
+//         wordinvertida += word[index]
+        
+        
+// }       
+// console.log(wordinvertida); 
+////////////Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor.
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = array[0]
+// let menorPalavra = array[0]
+//     // for (let index = 1; index < array.length; index++) {
+//     //     if (array[index].length > maiorPalavra.length) {
+//     //         maiorPalavra = array[index]
+//     //     }
+        
+//     // }console.log(maiorPalavra);
+//     for (let index = 1; index < array.length; index++) {
+//         if (array[index].length < menorPalavra.length) {
+//             menorPalavra = array[index]
+//         }
+        
+//     }console.log(menorPalavra);
+////////////////Escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+
+////////////////
+    // let player = {
+    //     name: "Marta",
+    //     lastName: " Silva",
+    //     age: 34,
+    //     medals: {
+    //         golden: 2,
+    //         silver: 3,
+    //     },
+    //     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+    // } 
+
+    // console.log("A jogadora " + player.name + player.lastName + " tem " + player.age + " anos de idade.");
+    // console.log("A jogadora " + player.name + player.lastName + " foi eleita a melhor do mundo por " + 
+    // player.bestInTheWorld.length + " vezes.");
+    // console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + 
+    // " medalhas de prata.");
